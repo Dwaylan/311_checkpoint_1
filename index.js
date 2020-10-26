8da539a90ea0ee17d3b3133efe86b1a0246aa0c0
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-26 15:21:26 -0500

    early stages of checkpoint

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// access to the users file of the route folder
+const users = require("./routes/users");
 
-app.get('/', (req, res) => res.send('default route'))
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(users);
+
+app.get("/", (req, res) => res.send("default route"));
 
 app.listen(port, () => {
-  console.log('app is listening on:', port)
-})
+  console.log("app is listening on:", port);
+});
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// "Routes" to forward the supported HTTP requests to the appropriate controller functions.
+
+module.exports = router;
