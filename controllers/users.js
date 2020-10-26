c29b3db3488792875ba86e03caed728aad7255f9
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-26 16:55:08 -0500

    working on controllers and saving my work in increments

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// We need to retrieve the users by accessing the index file in the data folder
+const users = require("..data/index");
+
+// We will create five controller functions. These will correspond to the routes.
+// ALL LOGIC for retrieving or updating the "data" should be done here.
+// After that is complete we will import these controller functions into the routes.
+
+// listUsers should retrieve the entire array from data/index
+const listUsers = 
+
+// showUser should retrieve just the user that matches the passed-in id
+const showUser =
+
+
+// createUser should add a user to the array 
+
+// updateUser should update a user from the array based on its id
+
+// deleteUser should delete a user from the array based on its id
+
+module.exports("controllers");
