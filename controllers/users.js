d81bfcc4c915b4b0737fe8f1d6fa0dc81da393d6
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-27 18:08:05 -0500

    worked with my classmates to figure out error handling and a few syntax kinks. Couldn't get nodemon to fire off and realized it was a bracketing issue

--- controllers/users.js
+++ controllers/users.js
@@ -9,18 +9,33 @@ const users = require("../data/index");
 // listUsers should retrieve the entire array from data/index
 const listUsers = (req, res) => {
   res.json(users);
+  if (!users) {
+    res.status(404).send("user not found");
+    return;
+  }
 };
 
 // showUser should retrieve just the user that matches the passed-in id
 const showUser = (req, res) => {
   let userId = parseInt(req.params.userId);
   let user = users.find((user) => user._id === userId);
-  res.send(user);
+  //  If there is no user to be found, throw up erro code 404
+  if (!user) {
+    res.status(404).send("users not found");
+    return;
+  } else {
+    res.send(user);
+  }
 };
 
 // createUser should add a user to the array
 const createUser = (req, res) => {
   let payload = req.body;
+
+  if (payload.id == undefined || payload.id == false) {
+    res.status(404).send("error");
+    return;
+  }
   payload.id = users.length + 1;
   users.push(payload);
   res.send(users);
@@ -28,11 +43,38 @@ const createUser = (req, res) => {
 
 // updateUser should update a user from the array based on its id
 const updateUser = (req, res) => {
-  let id = parseInt(req.params.id);
+  let userId = parseInt(req.params.id) - 1;
+  let user = users.find((user) => user.id === userId);
   let payload = req.body;
+  if (user.length < userId) {
+    res.status(404).send("bad request here");
+    return;
+  } else {
+    Object.keys(payload).map((key) => {
+      user[key] = payload[key];
+    });
+    payload.id = user[id].id - 1;
+    res.send(user);
+  }
 };
 
 // deleteUser should delete a user from the array based on its id
-const deleteUser = (req, res) => {};
+const deleteUser = (req, res) => {
+  let userId = parseInt(req.params.id) - 1;
+  let user = users.find((user) => user.id === userId);
 
-module.exports(listUsers, showUser, createUser, updateUser, deleteUser);
+  if (!user) {
+    res.status(404).send("bad request here");
+    return;
+  } else {
+    users.splice(user.id, 1);
+    users.forEach((element) => {
+      if (element.id > user.id) {
+        element.id -= 1;
+      }
+    });
+    res.send(users);
+  }
+};
+
+module.exports = { listUsers, showUser, createUser, updateUser, deleteUser };
